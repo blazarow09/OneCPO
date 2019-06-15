7418c2bcbb3b68eb120a943c07f64d453b325c53
blazarow09 <blazarow09@gmail.com>
2019-06-15 20:03:54 +0300

    upload customers view model

---
+++ src/ViewModels/OneCPO.ViewModels/Output/CustomersViewModel.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using OneCPO.Data.Models;
+using OneCPO.Data.Models.Enums;
+using OneCPO.Services.Mapping.Contracts;
+using System;
+
+namespace OneCPO.ViewModels.Output
+{
+    public class CustomersViewModel : IMapFrom<Customer>, IHaveCustomMappings
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public GenderType Gender { get; set; }
+
+        public string Telephone { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public StatusType Status { get; set; }
+
+        //TODO check for purchase orders
+
+        public void CreateMappings(IMapperConfigurationExpression configuration)
+        {
+            //configuration.CreateMap<Customer, CustomersViewModel>()
+            //    .ForMember(x => x.CountOfAllJokes,
+            //        m => m.MapFrom(c => c.Jokes.Count()));
+        }
+    }
+}
