3953b7fa7cb7de9bd73e72dc6cae1b4aea94f90d
blazarow09 <blazarow09@gmail.com>
2019-06-15 22:46:20 +0300

    upload create purchase model

---
+++ src/ViewModels/OneCPO.ViewModels/Input/PurchaseOrder/CreatePurchaseOrderModel.cs
@@ -0,0 +1,13 @@
+﻿namespace OneCPO.ViewModels.Input.PurchaseOrder
+{
+    public class CreatePurchaseOrderModel
+    {
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int Customer { get; set; }
+    }
+}
