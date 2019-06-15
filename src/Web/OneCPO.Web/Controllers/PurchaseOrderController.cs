6a0378983e7eb1b883bbb38e827071cb8d352b47
blazarow09 <blazarow09@gmail.com>
2019-06-15 22:02:16 +0300

    upload purchase order controller with implemented index view action

---
+++ src/Web/OneCPO.Web/Controllers/PurchaseOrderController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OneCPO.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneCPO.Web.Controllers
+{
+    public class PurchaseOrderController : Controller
+    {
+        private readonly IPurchaseOrderService purchaseOrderService;
+
+        public PurchaseOrderController(IPurchaseOrderService purchaseOrderService)
+        {
+            this.purchaseOrderService = purchaseOrderService;
+        }
+
+        public IActionResult Index()
+        {
+            var purchases = this.purchaseOrderService.GetAllPurchases();
+
+            return this.View(purchases);
+        }
+    }
+}
