67137dc8f69b74b3b2445adfb1a79a4e6ac6933f
blazarow09 <blazarow09@gmail.com>
2019-06-15 21:09:29 +0300

    added dbset models

--- src/Data/OneCPO.Data/OneCPODbContext.cs
+++ src/Data/OneCPO.Data/OneCPODbContext.cs
@@ -1,16 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OneCPO.Data.Models;
 
 namespace OneCPO.Data
 {
     public class OneCPODbContext : DbContext
     {
-        public OneCPODbContext()
-        {
-        }
-
         public OneCPODbContext(DbContextOptions<OneCPODbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<PurchaseOrder> PurchaseOrders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
