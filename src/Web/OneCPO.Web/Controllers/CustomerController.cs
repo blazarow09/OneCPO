515bdd801fbe3264b8ab8c04b12e8e410d5b3c94
blazarow09 <blazarow09@gmail.com>
2019-06-15 21:41:07 +0300

    implemented remove, activate, deactivate and info actions

--- src/Web/OneCPO.Web/Controllers/CustomerController.cs
+++ src/Web/OneCPO.Web/Controllers/CustomerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using OneCPO.Data.Models;
 using OneCPO.Services.Contracts;
 using OneCPO.ViewModels.Input.Customer;
 using ReflectionIT.Mvc.Paging;
@@ -53,6 +54,54 @@ namespace OneCPO.Web.Controllers
             this.customerService.Create(customer);
 
             return RedirectToAction("Index", "Customer");
+        }
+
+        [HttpGet]
+        public IActionResult Remove(int id)
+        {
+            this.customerService.DeleteUser(id);
+
+            return RedirectToAction("Index", "Customer");
+        }
+
+        [HttpGet]
+        public IActionResult Activate(int id)
+        {
+            this.customerService.Activate(id);
+
+            return RedirectToAction("Index", "Customer");
+        }
+
+        [HttpGet]
+        public IActionResult Deactivate(int id)
+        {
+            this.customerService.Deactivate(id);
+
+            return RedirectToAction("Index", "Customer");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var customer = this.customerService.GetSingleCustomer(id);
+
+            return this.View(customer);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Customer input)
+        {
+            this.customerService.EditCustomer(input);
+
+            return RedirectToAction("Info", "Customer", input);
+        }
+
+        [HttpGet]
+        public IActionResult Info(int id)
+        {
+            var customer = this.customerService.GetSingleCustomer(id);
+
+            return this.View(customer);
         }
     }
 }
